a8435e181eaa1551008b9cfc652bc19e8d71f6c2
EscuderoCSaantiago <133914125+EscuderoCSaantiago@users.noreply.github.com>
2023-06-21 09:47:40 -0600

    Add files via upload

---
+++ jhgfUITests/jhgfUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  jhgfUITestsLaunchTests.swift
+//  jhgfUITests
+//
+//  Created by santiago escudero on 12/06/23.
+//
+
+import XCTest
+
+final class jhgfUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
